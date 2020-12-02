7f8beeb821336e6493d248d2f511c5ec75ff4d0b
Sébastien Willmann <swillmann@zeenea.com>
2020-12-02 20:58:51 +0100

    Day 2 Part 2

--- Day2.hs
+++ Day2.hs
@@ -23,6 +23,15 @@ check rule s =
     let l = length (filter (== (letter rule)) s)
     in l >= minLetters rule && l <= maxLetters rule
 
+xor :: Bool-> Bool-> Bool
+xor True True = False
+xor True False = True
+xor False True = True
+xor False False = False
+
+check2 :: PasswordRule -> String -> Bool
+check2 rule s = (s !! (minLetters rule - 1) == letter rule) `xor` (s !! (maxLetters rule - 1) == letter rule)
+
 solveLine :: String -> Bool
 solveLine "" = False
 solveLine s = check (readRule s) (readPassword s)
@@ -30,8 +39,16 @@ solveLine s = check (readRule s) (readPassword s)
 solve :: String -> Int
 solve = length . (filter solveLine) . lines
 
+solveLine2 :: String -> Bool
+solveLine2 "" = False
+solveLine2 s = check2 (readRule s) (readPassword s)
+
+solve2 :: String -> Int
+solve2 = length . (filter solveLine2) . lines
+
 runDay2 :: IO ()
 runDay2 = 
     do
         x <- readFile "passwords"
         print $ solve x
+        print $ solve2 x
